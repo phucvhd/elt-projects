583c1b8d1666490895080a7798b54fb8ff7b6b24
vuhoangdinhphuc <phucvhd@gmail.com>
2025-04-25 14:53:39 +0700

    Add feature to load raw json into db

--- youtube-trending/elt/elt.py
+++ youtube-trending/elt/elt.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+
 from youtube_client import YoutubeApiClient
+from sqlalchemy import create_engine, MetaData, Table, insert
 
 # Load environment variables
 load_dotenv()
@@ -8,6 +11,11 @@ api_key = os.getenv("GOOGLE_API_KEY")
 base_url = os.getenv("GOOGLE_DATA_V3_URL")
 max_results = os.getenv("MAX_RESULT")
 region_code = os.getenv("REGION_CODE")
+username = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOST")
+port = os.getenv("POSTGRES_PORT")
+dbname = os.getenv("POSTGRES_DB")
 
 # Init Youtube client
 yt_client = YoutubeApiClient(api_key=api_key,
@@ -22,3 +30,16 @@ if responses:
             title = video["snippet"]["title"]
             views = video["statistics"].get("viewCount", "N/A")
             print(f"{title} - {views} views")
+
+# Load data
+try:
+    engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}')
+    metadata = MetaData()
+    raw_youtube_trending_table = Table("raw_youtube_trending", metadata, autoload_with=engine)
+    with engine.connect() as conn:
+        stmt = insert(raw_youtube_trending_table).values(raw_json=responses)
+        conn.execute(stmt)
+        conn.commit()
+    print(f"✅ Data loaded successfully into '{dbname}'")
+except Exception as e:
+    print("❌ Failed to load data:", e)
