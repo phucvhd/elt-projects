621c8a833b3d980347f1de084cf6b053194bdfbc
vuhoangdinhphuc <phucvhd@gmail.com>
2025-04-25 17:01:59 +0700

    Add Load video_categories

--- youtube-trending/elt/elt.py
+++ youtube-trending/elt/elt.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker
 
 from youtube_client import YoutubeApiClient
-from sqlalchemy import create_engine, MetaData, Table, insert
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.dialects.postgresql import insert
 
 # Load environment variables
 load_dotenv()
@@ -24,7 +24,8 @@ yt_client = YoutubeApiClient(api_key=api_key,
                              base_url=base_url)
 
 # Extract data
-responses = yt_client.get_trending_videos(chart="mostPopular")
+trending_videos_response = yt_client.get_trending_videos(chart="mostPopular")
+video_categories_response = yt_client.get_video_categories()
 
 # Load data
 try:
@@ -32,9 +33,29 @@ try:
     metadata = MetaData()
     raw_youtube_trending_table = Table("raw_youtube_trending", metadata, autoload_with=engine)
     with engine.connect() as conn:
-        stmt = insert(raw_youtube_trending_table).values(raw_json=responses)
+        stmt = insert(raw_youtube_trending_table).values(raw_json=trending_videos_response)
         conn.execute(stmt)
         conn.commit()
-    print(f"✅ Data loaded successfully into '{dbname}'")
+    print(f"✅ Data raw_youtube_trending loaded successfully into '{dbname}'")
+except Exception as e:
+    print("❌ Failed to load data:", e)
+
+# Load video_categories
+try:
+    engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}')
+    # if engine.dialect.has_table(engine.connect(), "video_categories_lookup"):
+    #     print("Table exists!")
+    metadata = MetaData()
+    video_categories_lookup_table = Table("video_categories_lookup", metadata, autoload_with=engine)
+    with engine.connect() as conn:
+        for item in video_categories_response.get("items", []):
+            stmt = insert(video_categories_lookup_table).values(category_name=item["snippet"]["title"], category_id=item["id"])
+            stmt = stmt.on_conflict_do_update(
+                index_elements=["category_id"],
+                set_={"category_name": item["snippet"]["title"]}
+            )
+            conn.execute(stmt)
+        conn.commit()
+    print(f"✅ Data video_categories_lookup loaded successfully into '{dbname}'")
 except Exception as e:
     print("❌ Failed to load data:", e)
--- youtube-trending/elt/youtube_client.py
+++ youtube-trending/elt/youtube_client.py
@@ -17,7 +17,7 @@ class YoutubeApiClient:
             "key": self.api_key
         }
 
-        response = requests.get(self.base_url, params=params)
+        response = requests.get(self.base_url + "videos", params=params)
 
         if response.status_code == 200:
             return response.json()
@@ -25,6 +25,23 @@ class YoutubeApiClient:
             if response.status_code == 429:
                 print("Rate limit hit, sleeping...")
                 time.sleep(60)  # or longer depending on your quota window
-            print(f"Error: {response.status_code}")
+            print(f"Error from get_trending_videos: {response.status_code}")
+            print(response.text)
+            return None
+
+    def get_video_categories(self):
+        params = {
+            "part": "snippet",
+            "regionCode": self.region_code,
+            "key": self.api_key
+        }
+        response = requests.get(self.base_url + "videoCategories", params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            if response.status_code == 429:
+                print("Rate limit hit, sleeping...")
+                time.sleep(60)  # or longer depending on your quota window
+            print(f"Error from get_video_categories: {response.status_code}")
             print(response.text)
             return None
