de431d9299ca9fb82f5e99b6881c84c1695721b2
SteveVu <phucvhd@gmail.com>
2025-05-14 14:50:34 +0700

    Create video_stats table

---
+++ youtube-trending/models/yt_base_models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, JSON, TIMESTAMP, func, Text, String, Date, ForeignKey, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class RawYoutubeTrending(Base):
+    __tablename__ = 'raw_youtube_trending'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    raw_json = Column(JSON, nullable=False)
+    fetch_timestamp = Column(TIMESTAMP, server_default=func.now())
+
+class VideoCategory(Base):
+    __tablename__ = 'video_categories'
+    category_id = Column(String, primary_key=True)
+    category_name = Column(Text, nullable=False)
+
+class ChannelInfo(Base):
+    __tablename__ = 'channel_infos'
+    channel_id = Column(String, primary_key=True)
+    channel_title = Column(Text, nullable=False)
+    country = Column(Text, nullable=False)
+    published_at = Column(TIMESTAMP, nullable=False)
+
+class VideoStats(Base):
+    __tablename__ = 'video_stats'
+    video_id = Column(String, primary_key=True)
+    title = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    category_id = Column(String, ForeignKey('video_categories.category_id'), nullable=False)
+    channel_id = Column(String, ForeignKey('channel_infos.channel_id'), nullable=False)
+    published_at = Column(TIMESTAMP, nullable=False)
+    trending_date = Column(Date, nullable=False, server_default=text('CURRENT_DATE'))
+    view_count = Column(Integer, nullable=False)
+    like_count = Column(Integer, nullable=False)
+    comment_count = Column(Integer, nullable=False)
+    channel_infos = relationship("ChannelInfo")
+    video_categories = relationship("VideoCategory")
+
+
