8a81d9c74cb52ffba28f7ae829a04f3b4d9b45ff
SteveVu <phucvhd@gmail.com>
2025-06-07 15:53:59 +0700

    Refactor video stats to have composite key

--- youtube-trending/airflow/elt/helper/mapper.py
+++ youtube-trending/airflow/elt/helper/mapper.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from elt.models.yt_base_models import ChannelInfo, VideoCategory, VideoStats
 
 
@@ -11,13 +13,14 @@ def map_to_video_category(video_categories_item) -> VideoCategory:
     return VideoCategory(category_name=video_categories_item["snippet"]["title"],
                          category_id=video_categories_item["id"])
 
-def map_to_video_stats(trending_videos_item) -> VideoStats:
+def map_to_video_stats(trending_videos_item, trending_date: date) -> VideoStats:
     return VideoStats(video_id=trending_videos_item["id"],
                       title=trending_videos_item["snippet"]["title"],
                       description=trending_videos_item["snippet"]["description"],
                       category_id=trending_videos_item["snippet"]["categoryId"],
                       channel_id=trending_videos_item["snippet"]["channelId"],
                       published_at=trending_videos_item["snippet"]["publishedAt"],
+                      trending_date=trending_date,
                       view_count=trending_videos_item["statistics"]["viewCount"] if 'viewCount' in trending_videos_item['statistics'] else 0,
                       like_count=trending_videos_item["statistics"]["likeCount"] if 'likeCount' in trending_videos_item['statistics'] else 0,
                       comment_count=trending_videos_item["statistics"]["commentCount"] if 'commentCount' in trending_videos_item['statistics'] else 0)
