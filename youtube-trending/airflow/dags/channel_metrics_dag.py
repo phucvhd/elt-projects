79b70cf35a2dce8a7cd58588068b1bef0cbf822d
SteveVu <phucvhd@gmail.com>
2025-09-15 19:04:23 -0700

    Migrate airlfow standalone to api-server

--- youtube-trending/airflow/dags/channel_metrics_dag.py
+++ youtube-trending/airflow/dags/channel_metrics_dag.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import Param
@@ -7,13 +7,6 @@ from airflow.sdk.definitions.param import ParamsDict
 
 from airflow import DAG
 from elt.yt_videos import extract_channel_statistics, extract_channel_info_from_ids
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-}
 
 def fail_task():
     sys.exit(1)
@@ -26,28 +19,32 @@ def extract_channel_statistics_task(**context):
     channel_ids = context["params"]["channel_ids"]
     extract_channel_statistics(channel_ids)
 
-dag = DAG(
-    'Channel_Metrics',
-    default_args=default_args,
-    description='Channel metrics',
+with DAG(
+    "Channel_Metrics",
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+    },
+    description="Channel metrics",
+    schedule=timedelta(days=1),
     start_date=datetime.today(),
-    schedule=None,
     catchup=False,
     params=ParamsDict({
         "channel_ids": Param(type="array", items={"type": "string"})
     })
-)
+) as dag:
+    t1 = PythonOperator(
+        task_id='extract_channel_info',
+        python_callable=extract_channel_info_task,
+        dag=dag,
+    )
 
-t1 = PythonOperator(
-    task_id='extract_channel_info',
-    python_callable=extract_channel_info_task,
-    dag=dag,
-)
+    t2 = PythonOperator(
+        task_id='extract_channel_statistics',
+        python_callable=extract_channel_statistics_task,
+        dag=dag,
+    )
 
-t2 = PythonOperator(
-    task_id='extract_channel_statistics',
-    python_callable=extract_channel_statistics_task,
-    dag=dag,
-)
-
-t1 >> t2
+    t1 >> t2
